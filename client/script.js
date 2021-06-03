d721033fb466ae8f45ee618d864b03765c41bf46
gh3s <gh3s@protonmail.ch>
2021-06-03 14:43:24 -0300

    good tuesday

--- client/script.js
+++ client/script.js
@@ -1,6 +1,5 @@
-var handleFetchResult = function(result) {
-  console.log(result)
-  if (!result.ok) {
+var handleFetchResult = function (result) {
+   if (!result.ok) {
     return result.json().then(function(json) {
       if (json.error && json.error.message) {
         throw new Error(result.url + ' ' + result.status + ' ' + json.error.message);
@@ -14,54 +13,84 @@ var handleFetchResult = function(result) {
 };
 
 // Create a Checkout Session with the selected plan ID
-var createCheckoutSession = function(priceId) {
-  return fetch("http://127.0.0.1:4242/create-checkout-session", {
-    method: "POST",
+var createCheckoutSession = function (priceId, qty) {
+  return fetch("http://localhost:4242/create-checkout-session", {
+    method: "post",
     headers: {
+      'Accept': 'application/json, text/plain, */*',
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      priceId: priceId
+      "priceId": "price_1IuaN8GeWwMQD9UuVR5dCs8x",
+      "customer": "cus_JawG5l86crtEt3",
     })
-  }).then(handleFetchResult);
+  })
+  .then(handleFetchResult)
+  .catch(e=>console.error(e))
+  ;
+};
+
+var createUpdateSession = function (priceId, qty) {
+  return fetch("http://localhost:4242/update-checkout-session", {
+    method: "post",
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "priceId": "price_1IuaN8GeWwMQD9UuVR5dCs8x",
+      "customer": "cus_JawG5l86crtEt3",
+      "quantity": 1,
+      "subscription": null
+    })
+  })
+  .then(handleFetchResult)
+  .catch(e=>console.error(e))
+  ;
 };
 
 // Handle any errors returned from Checkout
-var handleResult = function(result) {
+var handleResult = function (result) {
   if (result.error) {
     showErrorMessage(result.error.message);
   }
 };
 
-var showErrorMessage = function(message) {
+var showErrorMessage = function (message) {
   var errorEl = document.getElementById("error-message")
   errorEl.textContent = message;
   errorEl.style.display = "block";
 };
 
 /* Get your Stripe publishable key to initialize Stripe.js */
-fetch("http://127.0.0.1:4242/setup")
-  .then(data=>{
-    console.log(data)
-    handleFetchResult()})
-  .then(function(json) {
+fetch("http://localhost:4242/setup")
+  .then(handleFetchResult)
+  .then(function (json) {
+    console.log(json)
     var publishableKey = json.publishableKey;
     var basicPriceId = json.basicPrice;
-
-    console.log(json)
-
     var stripe = Stripe(publishableKey);
-    // Setup event handler to create a Checkout Session when button is clicked
+    
     document
-      .getElementById("basic-plan-btn")
-      .addEventListener("click", function(evt) {
-        createCheckoutSession(basicPriceId).then(function(data) {
-          // Call Stripe.js method to redirect to the new Checkout page
-          stripe
-            .redirectToCheckout({
-              sessionId: data.sessionId
-            })
-            .then(handleResult);
-        });
+    .getElementById("basic-plan-btn")
+    .addEventListener("click", function (evt) {
+      createCheckoutSession(basicPriceId)
+      .then(function (data) {
+        stripe
+          .redirectToCheckout({ sessionId: data.sessionId })
+          .then(handleResult);
       });
-  });
+    });
+
+    document
+    .getElementById("pro-plan-btn")
+    .addEventListener("click", function (evt) {
+      createUpdateSession(basicPriceId)
+      .then(function (data) {
+        stripe
+        .redirectToCheckout({ sessionId: data.sessionId })
+        .then(handleResult);
+      });
+    });
+
+  }); 
--- index.js
+++ index.js
@@ -2,24 +2,60 @@
 // Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
 const express = require('express');
 const app = express();
-const stripe = require('stripe')('sk_test_eggottvTJ9QkM4WOxzsylAln')
+const stripe = require('stripe')('sk_test_eggottvTJ9QkM4WOxzsylAln');
+var cors = require('cors');
 
-app.get("/setup", (req, res) => {
-  console.log('setup route')
-  res.send({
-    publishableKey: 'pk_test_7444YYZNspAxth2wFFLly2PH',
-    basicPrice: 'price_G0FvDp6vZvdwRZ',
-  });
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+app.get("/setup", async (req, res) => {
+  console.log('setup route');
+  try {
+    res.send({
+      publishableKey: "pk_test_7444YYZNspAxth2wFFLly2PH",
+      basicPrice: "price_1IuaN8GeWwMQD9UuVR5dCs8x",
+    });    
+  } catch (error) {
+    res.send(error)
+  }
 });
 
-app.post('/create-checkout-session', async (req, res) => {
-  const { priceId } = req.body;
+app.post('/update-checkout-session', async(req, res) => {
+  const { customer } = req.body;
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'setup',
+      customer: customer,
+      setup_intent_data: {
+        metadata: {
+          customer_id: 'cus_FOsk5sbh3ZQpAU',
+          subscription_id: 'sub_8epEF0PuRhmltU',
+        },
+      },
+      success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+      cancel_url: 'https://example.com/cancel',
+    });
+    res.send({ sessionId: session.id, });
+  } catch (error) {
+    res.status(400);
+    console.error(e)
+    return res.send({
+      error: {
+        message: e.message,
+      }
+    });    
+  }
+})
 
-  // See https://stripe.com/docs/api/checkout/sessions/create
-  // for additional parameters to pass.
+app.post('/create-checkout-session', async (req, res) => {
+  const { priceId, customer } = req.body;
+  console.log('checkout session')
   try {
     const session = await stripe.checkout.sessions.create({
       mode: 'subscription',
+      customer: customer,
       payment_method_types: ['card'],
       line_items: [
         {
@@ -34,25 +70,26 @@ app.post('/create-checkout-session', async (req, res) => {
       success_url: 'http://localhost:4242/order/sucess/success.html?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: 'https://example.com/canceled.html',
     });
-
-    app.post('/order/success', async (req, res) => {
-        const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
-        const customer = await stripe.customers.retrieve(session.customer);
-   
-        res.send(`<html><body><h1>Thanks for your order, ${customer.name}!</h1></body></html>`);
-    });
-    
-    res.send({
-      sessionId: session.id,
-    });
+    res.send({ sessionId: session.id });
   } catch (e) {
     res.status(400);
+    console.error(e)
     return res.send({
       error: {
         message: e.message,
       }
     });
   }
+});
+
+app.post('/list-user-card', async(req, res) => {
+  const subscription = await stripe.subscriptions.retrieve('sub_49ty4767H20z6a');
+})
+
+app.post('/order/success', async (req, res) => {
+  const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+  const customer = await stripe.customers.retrieve(session.customer);
+  res.send(`<html><body><h1>Thanks for your order, ${customer.name}!</h1></body></html>`);
 });
 
 app.listen(4242, () => console.log(`Node server listening at http://localhost:${4242}/`));
